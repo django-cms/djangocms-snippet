d73ee43f5302986fdffdf6614b679650a98693f9
Patrick Lauber <digi@treepy.com>
2014-02-04 11:19:03 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 djangocms-snippet
 =================
 
-HTML snippet plugin for django CMS
+A HTML snippet plugin for django CMS.
+
+
+Installation
+------------
+
+This plugin requires `django CMS` 3.0 or higher to be properly installed.
+
+* In your projects `virtualenv`_, run ``pip install djangocms-snippet``.
+* Add ``'djangocms_snippet'`` to your ``INSTALLED_APPS`` setting.
+* Run ``manage.py migrate djangocms_snippet``.
+
+Translations
+------------
+
+If you want to help translate the plugin please do it on transifex:
+
+https://www.transifex.com/projects/p/django-cms/resource/djangocms-snippet/
+
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from djangocms_link import __version__
+
+
+INSTALL_REQUIRES = [
+    #'Django-Select2',
+]
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Communications',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+]
+
+setup(
+    name='djangocms-link',
+    version=__version__,
+    description='Link Plugin for django CMS',
+    author='Divio AG',
+    author_email='info@divio.ch',
+    url='https://github.com/divio/djangocms-link',
+    packages=['djangocms_link', 'djangocms_link.migrations'],
+    install_requires=INSTALL_REQUIRES,
+    license='LICENSE.txt',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    zip_safe=False
+)
