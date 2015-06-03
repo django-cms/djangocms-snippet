369833dbfdf6d494c8177be2dde833f723a2ef76
David THENON <sveetch@gmail.com>
2015-06-04 00:23:22 +0200

    * Added a templatetag to use snippets out the CMS templates; * Added a slug field that is much more consistent to target a snippet using the templatetag; * Added South migration for the new 'slug' field; * Missing Django migration for the new 'slug' field; * Bump version to 1.6;

--- djangocms_snippet/admin.py
+++ djangocms_snippet/admin.py
@@ -2,6 +2,11 @@ from .models import Snippet
 from django.contrib import admin
 
 class SnippetAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('slug', 'name')
+    search_fields = ['slug', 'name']
+    prepopulated_fields = {"slug": ("name",)}
+    #formfield_overrides = {
+        #TextField: {'widget': CodeMirrorWidget(config_name='cms_snippet')},
+    #}
 
 admin.site.register(Snippet, SnippetAdmin)
