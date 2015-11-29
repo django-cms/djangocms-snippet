8b2ccfa083ccac81d9ba788bef11e917bf4c4bee
David THENON <sveetch@gmail.com>
2015-11-29 14:29:43 +0100

    Removed slug field feature from old South migration, instead add it from Django migration

---
+++ djangocms_snippet/migrations_django/0004_auto_alter_slug_unique.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('djangocms_snippet', '0003_auto_data_fill_slug'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='snippet',
+            name='slug',
+            field=models.SlugField(default=b'', unique=True, max_length=75, verbose_name='slug'),
+            preserve_default=True,
+        ),
+    ]
