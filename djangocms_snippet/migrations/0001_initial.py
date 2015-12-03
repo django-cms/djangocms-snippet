5391f0e06a1c56066722e49ff50ab78ed6864683
Martin Koistinen <mkoistinen@gmail.com>
2015-12-03 15:29:08 -0500

    Update locations of migrations to meet standards

--- djangocms_snippet/migrations/0001_initial.py
+++ djangocms_snippet/migrations/0001_initial.py
@@ -1,78 +1,40 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models, connection
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        table_names = connection.introspection.table_names()
-        if 'snippet_snippet' in table_names or 'cmsplugin_snippetptr' in table_names:
-            if 'cmsplugin_snippetptr' in table_names:
-                db.rename_table('cmsplugin_snippetptr', 'djangocms_snippet_snippetptr')
-            if 'snippet_snippet' in table_names:
-                db.rename_table('snippet_snippet', 'djangocms_snippet_snippet')
-        else:
-            # Adding model 'Snippet'
-            db.create_table(u'djangocms_snippet_snippet', (
-                (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-                ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
-                ('html', self.gf('django.db.models.fields.TextField')(blank=True)),
-                ('template', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
-            ))
-            db.send_create_signal(u'djangocms_snippet', ['Snippet'])
+    dependencies = [
+        ('cms', '__first__'),
+    ]
 
-            # Adding model 'SnippetPtr'
-            db.create_table(u'djangocms_snippet_snippetptr', (
-                (u'cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
-                ('snippet', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['djangocms_snippet.Snippet'])),
-            ))
-            db.send_create_signal(u'djangocms_snippet', ['SnippetPtr'])
-
-    def backwards(self, orm):
-        # Deleting model 'Snippet'
-        db.delete_table(u'djangocms_snippet_snippet')
-
-        # Deleting model 'SnippetPtr'
-        db.delete_table(u'djangocms_snippet_snippetptr')
-
-
-    models = {
-        'cms.cmsplugin': {
-            'Meta': {'object_name': 'CMSPlugin'},
-            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
-            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
-            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
-            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
-            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
-        },
-        'cms.placeholder': {
-            'Meta': {'object_name': 'Placeholder'},
-            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'slot': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
-        },
-        u'djangocms_snippet.snippet': {
-            'Meta': {'ordering': "['name']", 'object_name': 'Snippet'},
-            'html': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
-            'template': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'})
-        },
-        u'djangocms_snippet.snippetptr': {
-            'Meta': {'object_name': 'SnippetPtr', '_ormbases': ['cms.CMSPlugin']},
-            u'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
-            'snippet': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['djangocms_snippet.Snippet']"})
-        }
-    }
-
-    complete_apps = ['djangocms_snippet']
+    operations = [
+        migrations.CreateModel(
+            name='Snippet',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=255, verbose_name='name')),
+                ('html', models.TextField(verbose_name='HTML', blank=True)),
+                ('template', models.CharField(help_text='Enter a template (i.e. "snippets/plugin_xy.html") which will be rendered. If "template" is given, the contents of field "HTML" will be passed as template variable {{ html }} to the template. Else, the content of "HTML" is rendered.', max_length=50, verbose_name='template', blank=True)),
+            ],
+            options={
+                'ordering': ['name'],
+                'verbose_name': 'Snippet',
+                'verbose_name_plural': 'Snippets',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='SnippetPtr',
+            fields=[
+                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
+                ('snippet', models.ForeignKey(to='djangocms_snippet.Snippet')),
+            ],
+            options={
+                'verbose_name': 'Snippet',
+            },
+            bases=('cms.cmsplugin',),
+        ),
+    ]
