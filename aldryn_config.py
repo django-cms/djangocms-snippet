bdb82bf96e532af77569a7c156e05b4d00f9c852
Angelo Dini <angelo.dini@divio.ch>
2016-08-31 12:11:06 +0200

    added aldryn config

---
+++ aldryn_config.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from aldryn_client import forms
+
+
+class Form(forms.BaseForm):
+    editor_theme = forms.CharField(
+        'Custom editor theme, (e.g. "github", default: "twilight")',
+        required=False,
+    )
+    editor_mode = forms.CharField(
+        'Custom editor mode (e.g. "javascript", default: "html")',
+        required=False,
+    )
+    enable_search = forms.BooleanField(
+        'Enable snippet content to be searchable.',
+        required=False,
+        default=False,
+    )
+
+    def to_settings(self, data, settings):
+        if data['editor_theme']:
+            settings['DJANGOCMS_SNIPPET_THEME'] = data['editor_theme']
+        if data['editor_mode']:
+            settings['DJANGOCMS_SNIPPET_MODE'] = data['editor_mode']
+        if data['enable_search']:
+            settings['DJANGOCMS_SNIPPET_SEARCH'] = data['enable_search']
+        return settings
