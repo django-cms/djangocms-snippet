e4cf009ad76c045a86a5a35a878c3d959249d8e7
Adam Murray <adam-murray@hotmail.co.uk>
2021-09-14 15:47:04 +0100

    Made copy function test and started copy function

--- djangocms_snippet/cms_config.py
+++ djangocms_snippet/cms_config.py
@@ -5,6 +5,22 @@ from cms.app_base import CMSAppConfig
 from .models import Snippet
 
 
+def _get_model_fields(instance, model, field_exclusion_list=[]):
+    field_exclusion_list.append(model._meta.pk.name)
+    return {
+        field.name: getattr(instance, field.name)
+        for field in model._meta.fields
+        if field.name not in field_exclusion_list
+    }
+
+
+def snippet_copy_method(old_snippet):
+    old_snippet_fields = _get_model_fields(old_snippet, Snippet)
+    import pdb
+    pdb.set_trace()
+    new_snippet = Snippet.objects.create(**old_snippet_fields)
+
+
 class SnippetCMSAppConfig(CMSAppConfig):
     djangocms_versioning_enabled = getattr(
         settings, 'DJANGOCMS_SNIPPET_VERSIONING_ENABLED', False
---
+++ tests/test_config.py
@@ -0,0 +1,22 @@
+from cms.test_utils.testcases import CMSTestCase
+
+from djangocms_snippet.cms_config import snippet_copy_method
+
+from .utils.factories import SnippetWithVersionFactory
+
+
+class VersioningConfigTestCase(CMSTestCase):
+
+    def test_snippet_copy_method(self):
+        old_snippet = SnippetWithVersionFactory(
+            name="snippet",
+            html="<p>Hello World</p>",
+            slug="snippet",
+        )
+
+        new_snippet = snippet_copy_method(old_snippet)
+
+        self.assertEqual(old_snippet.name, new_snippet.name)
+        self.assertEqual(old_snippet.html, new_snippet.html)
+        self.assertEqual(old_snippet.snippet_grouper, new_snippet.snippet_grouper)
+        self.assertEqual(old_snippet.versions.count() + 1, new_snippet.versions.count())
