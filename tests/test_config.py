1a7377a08298e78a6ceefb672f87b187b41314ec
Adam Murray <adam-murray@hotmail.co.uk>
2021-09-16 11:20:04 +0100

    Added additional test criteria

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,7 @@
 from cms.test_utils.testcases import CMSTestCase
 
 from djangocms_snippet.cms_config import snippet_copy_method
+from djangocms_snippet.models import Snippet, SnippetGrouper
 
 from .utils.factories import SnippetWithVersionFactory
 
@@ -19,3 +20,5 @@ class VersioningConfigTestCase(CMSTestCase):
         self.assertEqual(old_snippet.name, new_snippet.name)
         self.assertEqual(old_snippet.html, new_snippet.html)
         self.assertEqual(old_snippet.snippet_grouper, new_snippet.snippet_grouper)
+        self.assertEqual(1, SnippetGrouper.objects.count())
+        self.assertEqual(2, Snippet.objects.count())
