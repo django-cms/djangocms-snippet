162156811a158fde091e8156a96c4f2f8c8a8bd5
bernard <bvdv9075@gmail.com>
2022-01-31 13:39:03 +0000

    isort and flake errors

--- djangocms_snippet/admin.py
+++ djangocms_snippet/admin.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.admin.exceptions import DisallowedModelAdminToField
-from django.contrib.admin.utils import (quote, unquote, flatten_fieldsets)
 from django.contrib.admin import helpers
+from django.contrib.admin.exceptions import DisallowedModelAdminToField
+from django.contrib.admin.utils import flatten_fieldsets, unquote
 from django.db import models
 from django.forms import Textarea
 from django.utils.translation import gettext as _
