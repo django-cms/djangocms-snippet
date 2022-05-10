dbd61f5583230247a62102ef222eca5768b437d8
Aiky30 <Aiky30@users.noreply.github.com>
2022-05-10 15:54:12 +0100

    feat: django-cms 4.0.x - django 3.2 and Python 3.9 compatibility  (#92)

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -4,8 +4,14 @@ Changelog
 
 Unreleased
 ==========
+
+* Python 3.8, 3.9 support added
+* Django 3.0, 3.1 and 3.2 support added
+* Python 3.5 and 3.6 support removed
+* Django 1.11 support removed
 * port-feat: pre-commit config added from the v3 workstream
 * fix: Added test coverage to admin preview view
+
 
 4.0.0.dev4 (2022-02-03)
 =======================
--- tests/settings.py
+++ tests/settings.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 HELPER_SETTINGS = {
+    'SECRET_KEY': "djangocmssnippetstestsuitekey",
     'INSTALLED_APPS': [
         'tests.utils',
         'djangocms_versioning',
@@ -18,6 +19,7 @@ HELPER_SETTINGS = {
     'CMS_TEMPLATES': (
         ("page.html", "Normal page"),
     ),
+    "DEFAULT_AUTO_FIELD": "django.db.models.AutoField",
 }
 
 
