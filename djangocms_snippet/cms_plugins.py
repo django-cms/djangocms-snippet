cecc189b8ad3e7b6553eb888e8ae8905fbff8af3
Fabian Braun <fsbraun@gmx.de>
2022-08-29 18:37:53 +0200

    Fix:		remove unused import

--- djangocms_snippet/cms_plugins.py
+++ djangocms_snippet/cms_plugins.py
@@ -7,7 +7,6 @@ from django.utils.translation import gettext_lazy as _
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
-from .forms import SnippetPluginForm
 from .models import SnippetPtr
 
 
