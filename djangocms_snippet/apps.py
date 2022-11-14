b11c7f3021f12af0cfb26d25206946ac4449c443
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2022-11-14 16:12:39 +0000

    fix: only check for missing migrations in this app
    Previously test would fail if there were missing migrations in apps in
    requirements. Also sets default_auto_field attribute on the app config
    to avoid unnecessary migration to be generated

--- djangocms_snippet/apps.py
+++ djangocms_snippet/apps.py
@@ -5,3 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class SnippetConfig(AppConfig):
     name = 'djangocms_snippet'
     verbose_name = _('Snippets')
+    default_auto_field = 'django.db.models.AutoField'
