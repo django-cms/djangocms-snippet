4245392f710ec2f17ed053dd3dd14e25e589f436
Mark Walker <mark@django-cms.org>
2023-10-26 09:50:16 +0100

    build: Move package in to src dir

---
+++ src/djangocms_snippet/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class SnippetConfig(AppConfig):
+    name = "djangocms_snippet"
+    verbose_name = _("Snippets")
