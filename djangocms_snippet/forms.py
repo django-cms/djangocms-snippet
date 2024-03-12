f736166af90ec30da6a1d1f01e9e0b5155b18cb8
傅瑞曼 <yxzfreeman@gmail.com>
2024-03-12 08:28:04 +0000

    init support for django4.2

--- djangocms_snippet/forms.py
+++ djangocms_snippet/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib import admin
 from django.db import transaction
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from cms.utils.urlutils import admin_reverse
 
