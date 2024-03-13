d16242384bc7324f1071aa980dd7fe5d7d47a781
傅瑞曼 <yxzfreeman@gmail.com>
2024-03-13 05:01:27 +0000

    fix ci failed issue

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
+import os
+import sys
 from setuptools import find_packages, setup
 
-from djangocms_snippet import __version__
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+from djangocms_snippet import __version__
+  
 
 REQUIREMENTS = [
     'django-cms',
